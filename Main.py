900ff4471b26a44af0e97426343d842332abc80f
Pritam Pattnaik <37960218+PritamPattnaik360@users.noreply.github.com>
2024-11-17 17:59:13 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import webbrowser
+
+st.subheader("What to look into?")
+st.write("""
+    You can find a nice overview of this project on the overview page.
+    When on the testing page when you predict house prices based on the model chosen you can see how the model
+    weights each factor given from the dataset.
+""")
+st.subheader("Learn more")
+st.write("""
+    Check out my github to see the specific jupiter notebook file on how I trained the model.
+""")
+if st.button("Open My Github"):
+    webbrowser.open_new_tab("https://github.com/PritamPattnaik360")
+
