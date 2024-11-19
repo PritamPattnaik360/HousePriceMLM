7ecca65a6a6f94752680c6c1b484d58d97f0d039
Pritam Pattnaik <37960218+PritamPattnaik360@users.noreply.github.com>
2024-11-19 14:21:17 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import webbrowser
 
-st.subheader("What to look into?")
+st.subheader("What is this?")
+st.write("""
+    This project predicts house prices using machine learning models, trained on a dataset containing features like area, number of bedrooms, and bathrooms. 
+    In Jupyter Notebook, I preprocessed the data (handling missing values, encoding categorical features), split it into training and testing sets, and trained 
+    models like Linear Regression, Random Forest, Decision Tree, and Neural Network, saving them with joblib for deployment
+""")
 st.write("""
     You can find a nice overview of this project on the overview page.
     When on the testing page when you predict house prices based on the model chosen you can see how the model
